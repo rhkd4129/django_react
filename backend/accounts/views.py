827569a8da066ca999aa03109601eac06f9696b8
Lee <rhkd4129@naver.com>
2022-04-05 22:40:04 +0900

    'signupAPiView'

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth import get_user_model
+from .serializers import SignupSerializer
+
+
+class SignUpView(CreateAPIView):
+    model  = get_user_model()
+    serializer_class = SignupSerializer
+    permission_classes = [
+        # permissions.AllowAny
+    ]
